71966b9b86f0d92a1e30dd50e21b255da54bc054
Arkdael <felixdesilets23@hotmail.com>
2026-07-02 15:39:00 -0400

    INIT Création du projet MVC

---
+++ SiteWebTransactionnel/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SiteWebTransactionnel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
